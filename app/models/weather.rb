9ea65e2b4c91b88f3b10b03c9039d5a1a706cf16
Wataru.Kasahara <kasahara1019@gmail.com>
2017-02-19 09:08:21 +0000

    気温情報を表示するように修正

--- app/models/weather.rb
+++ app/models/weather.rb
@@ -9,8 +9,16 @@ class Weather < ApplicationRecord
 
   def self.parse_msg(body)
     result = JSON.parse(body)
+    forecastList = result['forecasts']
+    temperatureInfo = "\n----------\n"
+    forecastList.each do |forecast|
+      temperatureInfo += "#{forecast['date']}(#{forecast['dateLabel']})"
+      temperatureInfo += "  天気 : #{forecast['telop']}\n"
+      temperatureInfo += "  気温 : 最高気温 #{forecast['temperature']['max']['celsius']}℃"   if forecast['temperature']['max']
+      temperatureInfo += " / " + " 最低気温 #{forecast['temperature']['min']['celsius']}℃\n" if forecast['temperature']['min']
+    end
     description = result['description']['text'].split((/\s*【東京地方】\s*/))
-    return result['title'] + "\n\n" + description[1]
+    return result['title'] + "\n\n " + description[1] + temperatureInfo
   end
 
   def self.error_msg(res)
