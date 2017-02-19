5265f5d36a081626a88edf435bfc52ff812bc5d5
Wataru.Kasahara <kasahara1019@gmail.com>
2017-02-19 06:10:04 +0000

    「天気」に反応して東京都の天気を返却する処理を追加

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -1,10 +1,12 @@
 require 'line/bot'
+require 'net/http'
+require 'uri'
 
 class LinebotController < ApplicationController
   def callback
     message = {
       type: 'text',
-      text: 'hello, LINE bot'
+      text: 'Hello, This is LINE bot'
     }
     client = Line::Bot::Client.new { |config|
       config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
@@ -18,10 +20,33 @@ class LinebotController < ApplicationController
       when Line::Bot::Event::Message
         case event.type
         when Line::Bot::Event::MessageType::Text
-          response = client.reply_message(event['replyToken'], message)
-          p response.body
+          case event['message']['text']
+          when '天気'
+            message['text'] = fetch_weather
+            response = client.reply_message(event['replyToken'], message)
+            p response.body
+          else
+            response = client.reply_message(event['replyToken'], message)
+            p response.body
+          end
         end
       end
+    end
+  end
+
+  def fetch_weather
+    uri = URI.parse('http://weather.livedoor.com/forecast/webservice/json/v1?city=130010')
+    http = Net::HTTP.new(uri.host, uri.port)
+    res = http.start {
+      http.get(uri.request_uri)
+    }
+    if res.code == '200'
+      result = JSON.parse(res.body)
+      puts result['title']
+      puts result['description']['text']
+      return result['title'] + "\n\n" + result['description']['text']
+    else
+      return "天気の取得に失敗しました #{res.code} #{res.message}"
     end
   end
 end
