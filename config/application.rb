86cac532304b93dd7eee72b154c6cfbb0e5a858e
Wataru.Kasahara <kasahara1019@gmail.com>
2017-02-19 09:11:24 +0000

    EagerLoad を設定

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module SwallowTail
   class Application < Rails::Application
+    config.paths.add 'lib', eager_load: true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
