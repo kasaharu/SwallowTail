77d9cd836c45d559877d0e573bc076bf23a03041
Wataru.Kasahara <kasahara1019@gmail.com>
2017-02-19 05:36:00 +0000

    LINE bot が反応するように処理を記載

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -1,4 +1,17 @@
 class LinebotController < ApplicationController
   def callback
+    message = {
+      type: 'text',
+      text: 'hello, LINE bot'
+    }
+    client = Line::Bot::Client.new { |config|
+        config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+        config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+    }
+
+    events = client.parse_events_from(request.body.read)
+
+    response = client.reply_message(events[0]['replyToken'], message)
+    p response
   end
 end
