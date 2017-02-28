8ea047d0a8ffd665d68abb825dc6106a09d194ed
Wataru.Kasahara <kasahara1019@gmail.com>
2017-02-28 13:29:19 +0000

    Devise のメール送信時のホスト名指定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  config.action_mailer.default_url_options = { host: ENV['DEVISE_HOST_NAME'], port: 3000 }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
