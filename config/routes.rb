cbc5076a9a9a909614f8c084813b2208513373e5
Wataru.Kasahara <kasahara1019@gmail.com>
2017-03-05 04:39:05 +0000

    Fix code style

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "home#index"
+  root to: 'home#index'
 
   get 'home/index'
   get 'home/show'
