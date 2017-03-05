b80d9598bff9a3d07e6b692cae18a500430dc3e0
Wataru.Kasahara <kasahara1019@gmail.com>
2017-03-05 16:42:00 +0900

    KeywordReply model の Strong Parameters を定義

--- app/controllers/keyword_replies_controller.rb
+++ app/controllers/keyword_replies_controller.rb
@@ -6,4 +6,9 @@ class KeywordRepliesController < ApplicationController
   def show
     @keyword_reply = KeywordReply.find(params[:id])
   end
+
+  private
+  def keyword_reply_params
+    params.require(:keyword_reply).permit(:keyword, :reply_type, :reply_word)
+  end
 end
