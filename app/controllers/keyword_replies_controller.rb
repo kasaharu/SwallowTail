c26951913a161f35923582a9b7ec5d2b950c9f89
Wataru.Kasahara <kasahara1019@gmail.com>
2017-03-05 17:03:50 +0900

    KeywordReply の新規追加機能の実装

--- app/controllers/keyword_replies_controller.rb
+++ app/controllers/keyword_replies_controller.rb
@@ -7,6 +7,24 @@ class KeywordRepliesController < ApplicationController
     @keyword_reply = KeywordReply.find(params[:id])
   end
 
+  def new
+    @keyword_reply = KeywordReply.new
+  end
+
+  def create
+    @keyword_reply = KeywordReply.new(keyword_reply_params)
+
+    respond_to do |format|
+      if @keyword_reply.save
+        format.html { redirect_to @keyword_reply, notice: 'KeywordReply was successfully created.' }
+        format.json { render :show, status: :created, location: @keyword_reply }
+      else
+        format.html { render :new }
+        format.json { render json: @keyword_reply.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
   def keyword_reply_params
     params.require(:keyword_reply).permit(:keyword, :reply_type, :reply_word)
