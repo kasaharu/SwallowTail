9d1ef53edd035728414f50e54e440b228cecb1af
Wataru.Kasahara <kasahara1019@gmail.com>
2017-03-05 16:24:19 +0900

    任意の KeywordReply データの表示機能を実装

--- app/controllers/keyword_replies_controller.rb
+++ app/controllers/keyword_replies_controller.rb
@@ -2,4 +2,8 @@ class KeywordRepliesController < ApplicationController
   def index
     @keyword_replies = KeywordReply.all
   end
+
+  def show
+    @keyword_reply = KeywordReply.find(params[:id])
+  end
 end
