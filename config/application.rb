2e375071962964846c265f951dc3defafe7dc11b
Wataru.Kasahara <kasahara1019@gmail.com>
2017-05-16 22:03:56 +0900

    lib 配下のファイルを読み込むための設定を変更

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(*Rails.groups)
 module SwallowTail
   class Application < Rails::Application
     config.paths.add 'lib', eager_load: true
+    config.autoload_paths += Dir["#{config.root}/lib/**/"]
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
