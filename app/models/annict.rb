8b9aa6a4ccc36ca4de66eb66c9c2f70660bf24e8
Wataru.Kasahara <kasahara1019@gmail.com>
2017-11-12 16:38:27 +0900

    Fix Lint error : Style/StringLiteralsInInterpolation

--- app/models/annict.rb
+++ app/models/annict.rb
@@ -1,6 +1,6 @@
 class Annict < ApplicationRecord
   def self.fetch(target_season)
-    uri_path = "https://api.annict.com/v1/works?fields=title&per_page=50&filter_season=#{target_season}&sort_watchers_count=desc&access_token=#{ENV["ANNICT_ACCESS_TOKEN"]}"
+    uri_path = "https://api.annict.com/v1/works?fields=title&per_page=50&filter_season=#{target_season}&sort_watchers_count=desc&access_token=#{ENV['ANNICT_ACCESS_TOKEN']}"
     return NetUtil.http_request(uri_path, true)
   end
 
