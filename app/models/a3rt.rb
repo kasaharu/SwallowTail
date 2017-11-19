e686f2c309a371ee39240a88e1e29c12fd6c1992
Wataru.Kasahara <kasahara1019@gmail.com>
2017-11-19 12:47:17 +0900

    Fix Lint Error : Layout/SpaceInsideHashLiteralBraces

--- app/models/a3rt.rb
+++ app/models/a3rt.rb
@@ -1,7 +1,7 @@
 class A3rt < ApplicationRecord
   def self.talk(message)
     uri_path = 'https://api.a3rt.recruit-tech.co.jp/talk/v1/smalltalk'
-    query_string = {'apikey' => ENV['A3RT_API_KEY'], 'query' => message}
+    query_string = { 'apikey' => ENV['A3RT_API_KEY'], 'query' => message }
 
     NetUtil.post_request(uri_path, query_string)
   end
